6c06e5e127935b70a9acc2f407369df065ec3f22
kuchichan <pawel.kucharski@hotmail.com>
2020-08-01 14:27:04 +0200

    Solved Aphabet Cipher

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,11 +1,59 @@
 (ns alphabet-cipher.coder)
 
+(defn string-to-int-array [str]
+  (->> (char-array str)
+       (seq)
+       (map int)
+       (map (partial + -97))
+       ))
+
+;; actually decoded ints and encoded ints could be a one function (+ / - difference)
+(defn calculate-encoded-ints [keyword message]
+  (->> (map string-to-int-array [message
+                                 (take (count message) (cycle keyword))])
+       (apply map +)
+       (map #(mod % 26))
+       (map (partial + 97))
+       ))
+
+(defn calculate-decoded-ints [keyword message]
+  (->> (map string-to-int-array [message
+                                 (take (count message) (cycle keyword))])
+       (apply map -)
+       (map #(mod % 26))
+       (map (partial + 97))
+       ))
+
+
+(defn int-array-to-string [xs]
+  (->> (map char xs)
+       (apply str)
+       ))
+
 (defn encode [keyword message]
-  "encodeme")
+  (->> (calculate-encoded-ints keyword message)
+       (int-array-to-string)
+       ))
+
 
 (defn decode [keyword message]
-  "decodeme")
+  (->> (calculate-decoded-ints keyword message)
+       (int-array-to-string))
+  )
+
+(defn take-substrings [string]
+  (map (partial subs string 0) (range 1 (-> string count inc)))
+  )
 
 (defn decipher [cipher message]
-  "decypherme")
-
+  (first (get 
+          (->> (map string-to-int-array [cipher message])
+               (apply map -)
+               (map #(mod % 26))
+               (map (partial + 97))
+               (int-array-to-string)
+               (take-substrings)
+               (group-by #(= message (decode % cipher)))
+               )
+          true))
+  )
